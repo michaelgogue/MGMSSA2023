c5e478b2eff0556056dbc2b929d398dffa6bbbce
Michael Gogue <Michael.gogue@outlook.com>
2023-06-19 18:21:57 -0700

    installing vscode and git from powershell gallery

---
+++ PowershellGallery install github and vscode.ps1
@@ -0,0 +1,7 @@
+﻿Install-Module -Name MSSAClassTools -force
+get-command -module MSSAClassTools
+
+Set-PreCourseConfigForSkillable
+Install-VSCodeAndGit
+
+# Username: Michaelgogue, Repository: MGMSSA2023, FulName: Michael Gogue, Email: michael.gogue@outlook.com
